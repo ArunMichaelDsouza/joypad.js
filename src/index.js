c10b7a603c88280cc5f386e6465010ad2eb5c9ef
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-05-31 02:21:50 +0530

    Gamepad connect/disconnect events registered

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 ; (function (w) {
     if (w.navigator.getGamepads) {
         console.log(w.navigator.getGamepads());
+
+        w.addEventListener('gamepadconnected', function () {
+            console.log('connected');
+        });
+
+        w.addEventListener('gamepaddisconnected', function () {
+            console.log('disconnected');
+        });
     }
 })(window);
