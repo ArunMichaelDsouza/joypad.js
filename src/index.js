22baf0a39fc9b4a675f2151035715c2344cb605e
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-01 04:17:47 +0530

    Basic pub/sub model implemented

--- src/index.js
+++ src/index.js
@@ -2,12 +2,59 @@
     if (w.navigator.getGamepads) {
         console.log(w.navigator.getGamepads());
 
-        w.addEventListener('gamepadconnected', function () {
-            console.log('connected');
+        w.addEventListener('gamepadconnected', function (e) {
+            emmitter.publish('gamepadconnected', e);
         });
 
-        w.addEventListener('gamepaddisconnected', function () {
-            console.log('disconnected');
+        w.addEventListener('gamepaddisconnected', function (e) {
+            emmitter.publish('gamepaddisconnected', e);
+        });
+
+        var emmitter = {
+            events: {},
+            publish: function (event, data) {
+                if (this.events.hasOwnProperty(event)) {
+                    this.events[event].forEach(function (listener) {
+                        listener(data);
+                    });
+                }
+            },
+            subscribe: function (event, listener) {
+                if (!this.events.hasOwnProperty(event)) {
+                    this.events[event] = [];
+                }
+                this.events[event].push(listener);
+
+                return {
+                    unsubscribe: function () {
+                        this.events[event].splice(this.events[event].indexOf(listener), 1);
+                    }.bind(this)
+                };
+            }
+        };
+
+        function Joypad() {
+            this.joypads = [];
+        }
+
+        Joypad.prototype.on = function (event, cb) {
+            switch (event) {
+                case 'connect':
+                    emmitter.subscribe('gamepadconnected', cb);
+                    break;
+                case 'disconnect':
+                    emmitter.subscribe('gamepaddisconnected', cb);
+                    break;
+            }
+        };
+
+        var joypad = new Joypad();
+
+        joypad.on('connect', function (e) {
+            console.log(e.type);
+        });
+        joypad.on('disconnect', function (e) {
+            console.log(e.type);
         });
     }
 })(window);
