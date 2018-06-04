f994c7b536a785d6688a2ed4461ad344f89233b8
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-05 02:27:58 +0530

    Request animation frame main function added

--- src/index.js
+++ src/index.js
@@ -45,16 +45,33 @@
                 case 'disconnect':
                     emmitter.subscribe('gamepaddisconnected', cb);
                     break;
+                case 'gamepadpress':
+                    emmitter.subscribe('gamepadpress', cb);
+                    break;
             }
         };
+
+        window.main = function () {
+            window.requestAnimationFrame(main);
+            var pressed = window.navigator.getGamepads()[0].buttons[0].pressed;
+            if (pressed) {
+                emmitter.publish('gamepadpress', pressed);
+            }
+        };
+
+        main();
 
         var joypad = new Joypad();
 
         joypad.on('connect', function (e) {
-            console.log(e.type);
+            console.log(e.gamepad);
+            window.g = e.gamepad;
         });
         joypad.on('disconnect', function (e) {
-            console.log(e.type);
+            console.log(e);
+        });
+        joypad.on('gamepadpress', function (e) {
+            console.log(e);
         });
     }
 })(window);
