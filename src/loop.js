91ebc0722fad82ef8d3d5299130b430af2100d2b
ArunMichaelDsouza <amdsouza92@gmail.com>
2019-05-06 00:33:46 +0530

    Method to update gamepad status added

--- src/loop.js
+++ src/loop.js
@@ -5,10 +5,24 @@ import { listenToButtonEvents } from './events';
 
 const loop = {
     id: null,
+    updateGamepadStatus: function () {
+        const gamepads = window.navigator.getGamepads();
+
+        if (Object.keys(gamepads).length) {
+            Object.keys(gamepads).forEach(i => {
+                const gamepad = gamepads[i];
+
+                if (gamepad) {
+                    joypad.list[i] = gamepad;
+                }
+            });
+        }
+    },
     start: function () {
         this.id = window.requestAnimationFrame(this.start.bind(this));
         joypad.loopStarted = true;
 
+        this.updateGamepadStatus();
         listenToButtonEvents(this.id);
     },
     stop: function (id) {
