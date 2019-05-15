b4027e06203ab4e5f45974fa11c155948abef8f1
ArunMichaelDsouza <amdsouza92@gmail.com>
2019-05-16 01:49:29 +0530

    Standard key mapping added

--- src/constants.js
+++ src/constants.js
@@ -42,5 +42,25 @@ const DIRECTIONS = {
     TOP: 'top',
     BOTTOM: 'bottom'
 };
+const KEY_MAPPING = {
+    'button_1': 0,
+    'button_2': 1,
+    'button_3': 2,
+    'button_4': 3,
+    'shoulder_left_1': 4,
+    'shoulder_right_1': 5,
+    'shoulder_left_2': 6,
+    'shoulder_right_2': 7,
+    'select': 8,
+    'start': 9,
+    'left_stick': 10,
+    'right_right': 11,
+    'd_pad_up': 12,
+    'd_pad_down': 13,
+    'd_pad_left': 14,
+    'd_pad_right': 15,
+    'vendor': 16,
+    'vendor': 17
+};
 
-export { EVENTS, BUTTON_PRESS_THRESHOLD, AXIS_MOVEMENT_THRESHOLD, STICKS, DIRECTIONS };
+export { EVENTS, BUTTON_PRESS_THRESHOLD, AXIS_MOVEMENT_THRESHOLD, STICKS, DIRECTIONS, KEY_MAPPING };
