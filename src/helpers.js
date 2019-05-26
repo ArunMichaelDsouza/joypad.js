423d2c27ffadff9c5a3a4582ac6996414b97ca1c
ArunMichaelDsouza <amdsouza92@gmail.com>
2019-05-26 17:40:43 +0530

    Test suite complete

--- src/helpers.js
+++ src/helpers.js
@@ -21,10 +21,10 @@ const findButtonMapping = (index, mapping) => {
     return results;
 };
 const hasVibrationSupport = vibrationActuator => {
-    return vibrationActuator && vibrationActuator.type && vibrationActuator.playEffect && typeof vibrationActuator.playEffect === 'function';
+    return vibrationActuator && vibrationActuator.type && vibrationActuator.playEffect && typeof vibrationActuator.playEffect === 'function' ? true : false;
 };
 const hasGamepadApiSupport = () => {
-    return window.navigator.getGamepads && typeof window.navigator.getGamepads === 'function';
+    return window.navigator.getGamepads && typeof window.navigator.getGamepads === 'function' ? true : false;
 };
 
 export { log, findButtonMapping, hasVibrationSupport, hasGamepadApiSupport };
--- test/joypad.test.js
+++ test/joypad.test.js
@@ -1,16 +1,61 @@
 // Tests for joypad.js
 
-import * as joypad from '../dist/joypad';
+import { EVENTS, AXIS_MOVEMENT_THRESHOLD } from '../src/constants';
+import emitter from '../src/emitter';
+import { listenToButtonEvents, listenToAxisMovements, handleButtonEvent } from '../src/events';
+import { hasVibrationSupport, hasGamepadApiSupport } from '../src/helpers';
+import joypad from '../src/joypad';
+import loop from '../src/loop';
 
 describe("joypad.js", () => {
-    let joypad;
+    let connectEventData;
+    let changedAxisMovementThreshold = 0.3;
 
     beforeAll(() => {
-        joypad = window.joypad;
+        connectEventData = { type: 'connect' };
+        joypad.instances = { 0: 'Gamepad' };
     });
 
-    test('Initialise', () => {
-        console.log(joypad);
+    test('Check data returned from emitter subscribed listener', () => {
+        emitter.subscribe(EVENTS.CONNECT.ALIAS, () => {
+            return connectEventData;
+        });
+
+        expect(emitter.events.connect[0].call().type === connectEventData.type).toEqual(true);
+    });
+
+    test('Check event methods', () => {
+        expect(typeof listenToButtonEvents).toEqual('function');
+
+        expect(typeof listenToAxisMovements).toEqual('function');
+
+        expect(typeof handleButtonEvent).toEqual('function');
+    });
+
+    test('Check helper methods', () => {
+        expect(hasVibrationSupport(null)).toEqual(false);
+
+        expect(hasGamepadApiSupport()).toEqual(false);
+    });
+
+    test('Check joypad instance', () => {
+        expect(joypad.loopStarted).toEqual(false);
+
+        expect(joypad.settings.axisMovementThreshold).toEqual(AXIS_MOVEMENT_THRESHOLD);
+
+        joypad.set({ axisMovementThreshold: changedAxisMovementThreshold });
+        expect(joypad.settings.axisMovementThreshold).toEqual(changedAxisMovementThreshold);
+
+        joypad.remove(0);
+        expect(joypad.instances).toEqual({});
+    });
+
+    test('Check main loop', () => {
+        expect(loop.id).toEqual(null);
+
+        expect(typeof loop.start).toEqual('function');
+
+        expect(typeof loop.stop).toEqual('function');
     });
 });
 
