6379fe23c615b5722f00812ca4b0e4b899069744
ArunMichaelDsouza <amdsouza92@gmail.com>
2019-06-06 01:40:17 +0530

    Joypad instance reset in no gamepad API support is there

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import { log, hasGamepadApiSupport } from './helpers';
 if (hasGamepadApiSupport()) {
     window.joypad = joypad;
 } else {
+    window.joypad = {};
     log('Your browser does not support the Gamepad API - https://developer.mozilla.org/en-US/docs/Web/API/Gamepad_API');
 }
