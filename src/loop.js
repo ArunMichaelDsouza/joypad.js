95ec9a841a430d8e2d01629ec33866a2ddd598e2
Arun Michael Dsouza <amdsouza92@gmail.com>
2019-11-27 01:22:11 +0000

    Get gamepads API update fix

--- src/loop.js
+++ src/loop.js
@@ -7,31 +7,28 @@ const loop = {
     id: null,
     start: function () {
         const requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
-        const gamepads = window.navigator.getGamepads();
         const { buttonEvents } = joypad;
+        let gamepads = window.navigator.getGamepads();
+        gamepads = Array.prototype.slice.call(gamepads);
 
         // Loop all the gamepads on each frame
-        if (Object.keys(gamepads).length) {
-            Object.keys(gamepads).forEach(index => {
-                const gamepad = gamepads[index];
-
-                if (gamepad) {
-                    // Initialise joypad instance events if not present
-                    if (!buttonEvents.joypad[index]) {
-                        buttonEvents.joypad[index] = {};
-                    }
-
-                    // Update gamepad instance data
-                    joypad.instances[index] = gamepad;
-
-                    // Listen to button press events
-                    listenToButtonEvents(gamepad);
-
-                    // Listen to axis movement events
-                    listenToAxisMovements(gamepad);
+        gamepads.forEach((gamepad, index) => {
+            if (gamepad) {
+                // Initialise joypad instance events if not present
+                if (!buttonEvents.joypad[index]) {
+                    buttonEvents.joypad[index] = {};
                 }
-            });
-        }
+
+                // Update gamepad instance data
+                joypad.instances[index] = gamepad;
+
+                // Listen to button press events
+                listenToButtonEvents(gamepad);
+
+                // Listen to axis movement events
+                listenToAxisMovements(gamepad);
+            }
+        });
 
         // Handle button events on each frame
         buttonEvents.joypad.forEach(events => {
